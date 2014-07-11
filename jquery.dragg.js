dfcf7cc0878bad5ae0261d2d7463f97d2c562b71
Tony Morello <tony@morellowebdesign.com>
2014-07-10 22:30:37 -0400

    Refactored
    Added 'destroy' method, refactored, cleaned up (kind of), improved drop
    detection, updated sample

--- jquery.dragg.js
+++ jquery.dragg.js
@@ -1,123 +1,147 @@
 (function( $ ){
 	
-	$.fn.dragg = function(options) {
+	var runDragg = function(options){
+				
+		var pY,
+			pX,
+			offset,
+			width,
+			height,
+			left,
+			top,
+			grabY,
+			grabX,
+			clone,
+			original,
+			mouse,
+			hover,
+			old_hover,
+			target;
 		
-		this.each(function(e){
-			dragg(this, options);
+		$(this).mousedown(function(e){
+			
+			e.preventDefault();
+			
+			if (e.target == this){
+				
+				if($.isFunction(options.onStart)) {
+					options.onStart.call(this);
+				}
+				
+				e.stopPropagation();
+				
+				mouse = 'down';
+				pY = e.pageY;
+				pX = e.pageX;
+				offset = $(this).offset();
+				width = $(this).width();
+				height = $(this).height();
+				
+				grabY = pY - offset.top;
+				grabX = pX - offset.left;
+				
+				original = $(this);
+				clone = $(this).clone();
+				
+				original.css('visibility' , 'hidden');
+							
+				clone.css({
+					'position' : 'absolute',
+					'z-index' : 1000,
+					'width' : width,
+					'left' : offset.left,
+					'top' : offset.top,
+				}).attr('id', options.helper).appendTo('body');
+				
+				$(this).trigger('dragstart');
+				
+				$('*').mousemove(function(e){
+					
+					e.preventDefault();
+					
+					if(mouse == 'down'){
+						pY = e.pageY;
+						pX = e.pageX;
+						left = pX - grabX;
+						top = pY - grabY;
+						
+						clone.hide();
+						old_hover = hover;
+						hover = document.elementFromPoint(pX, pY);
+						clone.show();
+						
+						clone.css({
+							'left' : left,
+							'top' : top
+						});
+						
+						if($.isFunction(options.onDrag)) {
+							options.onDrag.call(hover);
+						}
+						
+						$(clone).trigger('drag');
+						
+						if(old_hover!=hover){
+							$(hover).trigger('dragin');
+							$(old_hover).trigger('dragout');
+						}
+					}
+				});
+				
+				$(document).one('mouseup', function(e){
+					mouse = 'up';
+					if($.isFunction(options.onDrop)) {
+						options.onDrop.call($(hover));
+					}						
+					clone.remove();
+					original.css('visibility', '');
+					target = document.elementFromPoint(e.pageX, e.pageY);
+					$(target).trigger('drop');
+				});
+			
+			}
+						
 		});
 		
-		function dragg(element, options){
-			
-			var defaults = {
-				helper	:	'helper',
-				onStart	:	function(){},
-				onDrag	:	function(){},
-				onDrop	:	function(){}
-			}; 
-			
-			var options = $.extend({}, defaults, options); 
-			
-			var pY,
-				pX,
-				offset,
-				width,
-				height,
-				left,
-				top,
-				grabY,
-				grabX,
-				clone,
-				original,
-				mouse,
-				hover,
-				old_hover;
-			
-			$(element).mousedown(function(e){
-				
-				e.preventDefault();
-				
-				if (e.target == element){
-					
-					if($.isFunction(options.onStart)) {
-						options.onStart.call(element);
-					}
-					
-					e.stopPropagation();
-					
-					mouse = 'down';
-					pY = e.pageY;
-					pX = e.pageX;
-					offset = $(element).offset();
-					width = $(element).width();
-					height = $(element).height();
-					
-					grabY = pY - offset.top;
-					grabX = pX - offset.left;
-					
-					original = $(element);
-					clone = $(element).clone();
-					
-					original.css('visibility' , 'hidden');
-								
-					clone.css({
-						'position' : 'absolute',
-						'z-index' : 1000,
-						'width' : width,
-						'left' : offset.left,
-						'top' : offset.top,
-					}).attr('id', options.helper).appendTo('body');
-					
-					$(element).trigger('dragstart');
-					
-					$('body').mousemove(function(e){
-						
-						e.preventDefault();
-						
-						if(mouse == 'down'){
-							pY = e.pageY;
-							pX = e.pageX;
-							left = pX - grabX;
-							top = pY - grabY;
-							
-							clone.hide();
-							old_hover = hover;
-							hover = document.elementFromPoint(pX, pY);
-							clone.show();
-							
-							clone.css({
-								'left' : left,
-								'top' : top
-							});
-							
-							if($.isFunction(options.onDrag)) {
-								options.onDrag.call(hover);
-							}
-							
-							$(clone).trigger('drag');
-							
-							if(old_hover!=hover){
-								$(hover).trigger('dragin');
-								$(old_hover).trigger('dragout');
-							}
-						}
-					});
-					
-					$('body').one('mouseup', function(e){
-						mouse = 'up';
-						$(element).trigger('drop');
-						if($.isFunction(options.onDrop)) {
-							options.onDrop.call($(hover));
-						}						
-						clone.remove();
-						original.css('visibility', '');
-					});
-				
-				}
-							
-			});
-			
-		}
-				
 	}
+	
+	var methods = {
+		
+		init : function(options){
+			return this.each(function(e){
+				runDragg.call(this, options);
+			});
+		},
+		destroy: function(options) {
+			return $(this).each(function() {
+				$(this).off('mousedown');
+			});
+		}
+		
+	}
+	
+	$.fn.dragg = function(method, options) {
+		
+		if(!method || jQuery.isPlainObject(method) || !methods[method]){
+			options = method;
+			method = 'init';
+		}
+		
+		var defaults = {
+			helper	:	'helper',
+			onStart	:	function(){},
+			onDrag	:	function(){},
+			onDrop	:	function(){}
+		}; 
+		
+		var options = $.extend({}, defaults, options);
+		
+		return methods[method].call(this, options);
+		
+	}
+	
+	
+	
+	
 	
 })( jQuery );
